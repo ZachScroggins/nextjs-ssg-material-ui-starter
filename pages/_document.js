efec93d60006b598e30e1531abe9200130ff4bb1
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2020-05-25 04:46:17 -0500

    update w/ lessons learned from miniProjects

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,24 @@ import theme from '../src/theme';
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <html lang='en'>
         <Head>
+          <meta name='description' content='Your description' />
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width'
+          />
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name='theme-color' content={theme.palette.primary.main} />
           <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            rel='preconnect'
+            href='https://fonts.googleapis.com'
+            crossOrigin
+          />
+          <link rel='dns-prefetch' href='https://fonts.googleapis.com' />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
         </Head>
         <body>
@@ -24,7 +35,7 @@ export default class MyDocument extends Document {
   }
 }
 
-MyDocument.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async ctx => {
   // Resolution order
   //
   // On the server:
@@ -53,7 +64,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
@@ -61,6 +72,9 @@ MyDocument.getInitialProps = async (ctx) => {
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
   };
 };
--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,17 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-// import { red } from '@material-ui/core/colors';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
 // Create a theme instance.
-const theme = createMuiTheme({
-  // palette: {
-  //   primary: {
-  //     main: '#556cd6',
-  //   },
-  //   secondary: {
-  //     main: '#19857b',
-  //   },
-  //   error: {
-  //     main: red.A400,
-  //   },
-  //   background: {
-  //     default: '#fff',
-  //   },
-  // },
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
